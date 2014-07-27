f710833f0f76d9b061d30b1e9e9203430909c944
Marc Garreau <marcdgarreau@gmail.com>
2014-07-27 11:41:30 -0600

    Adds fundamental db connectivity to members

--- lib/models.rb
+++ lib/models.rb
@@ -1,19 +1,25 @@
 require 'rubygems'
 require 'sequel'
 
-Sequel::Model.plugin(:schema)
-DB = Sequel.sqlite('database.db')
+class Database
+  Sequel::Model.plugin(:schema)
+  DB = Sequel.sqlite('database.db')
 
-unless DB.table_exists? (:members)
-  DB.create_table :members do
-    primary_key :id
-    integer :membership_type_id
-    string :first_name, :null => false
-    string :last_name
-    string :phone_number
-    string :email_address
-    string :company
-    timestamp :joined_at
+  unless DB.table_exists? (:members)
+    DB.create_table :members do
+      primary_key :id
+      integer :membership_type_id
+      string :first_name, :null => false
+      string :last_name
+      string :phone_number
+      string :email_address
+      string :company
+      timestamp :joined_at
+    end
+  end
+
+  def self.membership
+    DB[:members]
   end
 end
 
