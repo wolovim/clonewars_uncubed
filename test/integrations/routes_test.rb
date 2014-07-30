98fd862bc695d8d07b4cbe413ab4be002c5b655e
Marc Garreau <marcdgarreau@gmail.com>
2014-07-30 16:46:15 -0600

    Adds routes test

--- test/integrations/routes_test.rb
+++ test/integrations/routes_test.rb
@@ -1,1 +1,43 @@
-# test routes
+require_relative "integration_test_helper"
+
+class RouteTester < FeatureTest
+  def test_can_view_index_page
+    visit '/'
+    assert_equal 200, page.status_code
+  end
+
+  def test_can_view_pricing_page
+    visit '/pricing'
+    assert_equal 200, page.status_code
+  end
+
+  def test_can_view_gallery_page
+    visit '/gallery'
+    assert_equal 200, page.status_code
+  end
+
+  def test_can_view_members_page
+    visit '/members'
+    assert_equal 200, page.status_code
+  end
+
+  def test_can_view_contact_page
+    visit '/contact-us'
+    assert_equal 200, page.status_code
+  end
+
+  def test_can_view_social_page
+    visit '/social'
+    assert_equal 200, page.status_code
+  end
+
+  def test_can_view_nearby_page
+    visit '/nearby'
+    assert_equal 200, page.status_code
+  end
+
+  def test_can_view_login_page
+    visit '/login'
+    assert_equal 200, page.status_code
+  end
+end
